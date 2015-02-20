4bc0ef9d8c43132c24379ffd02e0580fa5e43f20
RanaAlshemali <ra2834a@student.american.edu>
2015-02-20 12:24:55 -0500

    Create Romba.java

---
+++ Romba.java
@@ -0,0 +1,25 @@
+import javax.swing.JFrame;
+
+public class Romba {
+
+	static final int WIDTH = 1400, HIGHTS = 900;
+	 		RombaComponent rombacomponent;
+
+	public Romba() {
+		Integer[][] grid= new Integer[14][9];
+		JFrame window = new JFrame("Roomba");
+		window.setSize(WIDTH, HIGHTS);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		rombacomponent = new RombaComponent(this);
+		window.add(rombacomponent);
+		window.setVisible(true);
+		
+	
+	}
+
+	public static void main(String args[]) {
+		new Romba();
+	}
+
+
+}
