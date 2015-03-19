a9c2b93a9a17ba071a51579034a6938d43b45f5e
mikedeleamon <ma3971a@student.american.edu>
2015-03-19 01:21:31 -0400

    Update Romba.java

--- Romba.java
+++ Romba.java
@@ -1,5 +1,6 @@
 import javax.swing.JFrame;
-// this public class creates the window of the roomba project, the mouse listener, and creates and populates a grid
+/* this public class creates the window of the roomba project, 
+the mouse listener, and creates and populates a grid*/
 public class Romba {
 
 	static final int WIDTH = 1218, HIGHTS = 947; 	// size of window (with borders it is 1400,900)
@@ -13,20 +14,20 @@ public class Romba {
 		JFrame window = new JFrame("Roomba"); //names frame "Roomba"
 		window.setSize(WIDTH, HIGHTS);	// sets size of window
 		
-		for (int i = 0; i < 9; i++) // add nodes inside the 2D array
+		for (int i = 0; i < 9; i++) // adds nodes inside the 2D array
 			for (int j = 0; j < 12; j++)  
 				grid[i][j] = new gridObjects();
 							 
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // closes window when it is clicked
 		rombacomponent = new RombaComponent(this); // creates a roomba component for class 
-		roombaMListener = new RoombaMouseListener(this);// create mouse listener
-		rombacomponent.addMouseListener(roombaMListener);  // make the mouse active inside the window
-		window.add(rombacomponent);// add the rombacomponent to the window
-		window.setVisible(true); // set everything to be visible
+		roombaMListener = new RoombaMouseListener(this);// creates mouse listener
+		rombacomponent.addMouseListener(roombaMListener);  // makes the mouse active inside the window
+		window.add(rombacomponent);// adds the rombacomponent to the window
+		window.setVisible(true); // sets everything to be visible
 
 	}
 
 	public static void main(String args[]) {
-		new Romba(); // instantiate entire window class, creating window here
+		new Romba(); // instantiate the entire window class, creating window here
 	}
 }
